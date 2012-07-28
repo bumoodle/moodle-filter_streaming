f75bd8756341d5adef6e0d3e49caa85a5fdc852a
Kyle Temkin <ktemkin@binghamton.edu>
2012-07-27 22:43:50 -0400

    FILTER_JWPLAYERFILTER added video modifiers; much cleanup is still needed

---
+++ lang/en/filter_jwplayerfilter.php
@@ -0,0 +1,9 @@
+<?php
+ //$Id$// Language string for filter/jwfilter. 
+ $string['filtername'] = 'JWPlayerFilter';
+
+ $string['vmod_veryslow'] = 'Much Slower';
+ $string['vmod_slow'] = 'Slower';
+ $string['vmod'] = 'Normal';
+ $string['vmod_fast'] = 'Faster';
+ $string['vmod_veryfast'] = 'Much Faster';
